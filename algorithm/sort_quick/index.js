44bbcf9d6dec95b71ed490875f3855c66d146652
xinjian.huang <xinjian.huang@wormpex.com>
2019-04-26 20:37:08 +0800

    quick sort

---
+++ algorithm/sort_quick/index.js
@@ -0,0 +1,25 @@
+function sort(arr = []) {
+  if (!Array.isArray(arr) || arr.length === 1) {
+    return arr;
+  }
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[j] < arr[i]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  }
+  return arr;
+}
+const test1 = [5, 4, 3, 2, 1];
+const test2 = [1, 2, 3, 4, 5];
+const test3 = [1, 2, 3, 4, 3, 2, 1];
+const test4 = [3, 2, 1, 5, 6, 7, 3, 8, 6];
+console.log(sort(test1));
+console.log(sort(test2));
+console.log(sort(test3));
+console.log(sort(test4));
